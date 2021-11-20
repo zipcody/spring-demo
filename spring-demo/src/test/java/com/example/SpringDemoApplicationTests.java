14d3897b85a18e81e480b16441f19a0fdac66803
RHJ <hyunjooro@gmail.com>
2021-11-21 07:47:49 +0900

    initial commit

--- spring-demo/src/test/java/com/example/SpringDemoApplicationTests.java
+++ spring-demo/src/test/java/com/example/SpringDemoApplicationTests.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SpringDemoApplication {
 
 	public static void main(String[] args) {
-	SpringApplication.run(SpringDemoApplication .class,args);
+	SpringApplication.run(SpringDemoApplication.class, args);
 }
 
 @GetMapping(value = "/")
