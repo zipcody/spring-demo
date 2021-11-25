5e2e8b1e3bcc1edb78691f60f3b9aca134b7673d
zipcody <hello.git.hyunjooro@gmail.com>
2021-11-25 10:35:36 +0900

    initial commit

--- spring-demo/src/main/java/com/example/SpringDemoApplication.java
+++ spring-demo/src/main/java/com/example/SpringDemoApplication.java
@@ -2,12 +2,17 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringDemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDemoApplication.class, args);
 	}
-
+@GetMappping(value = "/")
+	public String HelloWorld(){
+		return "Hello World";
+}
 }
